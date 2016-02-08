69c78e9f195630fbf72c309e1fcab68bbcb54671
Mhl Mhlv <mhlv@Mhls-MacBook-Air.local>
2016-02-08 22:07:54 +0000

    here we go

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# worldsurfguide
+Фильтры НЕ работают!
+
+ТуДу фичи:
+геолокация + отъезд на мировой масштаб если спотов поблизости нет;
+всплывашки на кластерах и маркерах с краткой инфо + попапы (?) с подробностями по клику;
+карта с апрувленными спотами (для всех), с апрувленными и неапрувленными спотами и интерфейсом добавления спотов (для админов и редакторов);
+
+ТуДу по серверной части:
+стянуть базу спотов вот отсюда: wannasurf.com
+натравить на эту базу редакторов;
+Профит!
+
+ТуДу по структуре:
+добавить искусственные волны;
+решить, что делать с пиками;
+
+Примерная структура (требует обновления):
+
+параметры спота:
+name | str
+suitable swell size | from: int, to: int
+best season (month - month) | from: int, to: int
+best tide | string (f.e. mid-low, all-tide)
+avg temp. summer | int
+avg temp. winter | int
+suitable outfit | string (ws3, ws4, boardshorts, etc)
+peaks | int
+spot description | string
+surfer level | collection (rookie, intermediate, pro etc.)
+big wave spot | boolean
+infrastructure | array
+coordinates | lat + lon
+sharks rate | int
+Text coords | str
+
+параметры пика (peaks):
+avg.  size | int
+wave direction | string (left, right, a-frame)
+wave char. | string (steep, mellow, etc.)
+bottom char. | string (reef, sand, rocks, etc.)
+suitable boards | collection
+
+параметры инфраструктуры (bool):
+shower
+toilet
+food/drinks
+lifeguard / water patrol
+board rental
+surf school
+additional | string
+
+параметры фильтров:
+wave size | range
+season | range
+surfer level | range
+suit. outfit | checkbox collection
+suit. boards | checkbox collection
+wave direction | checkbox collection
+wave char. | checkbox collection
+bottom char. | checkbox collection
+infrastructure | checkbox collection
+big wave | checkbox
+no sharks | checkbox
---
+++ js/common.js
@@ -0,0 +1,173 @@
+var map;
+
+var months = {
+    1:"january",
+    2:"february",
+    3:"march",
+    4:"april",
+    5:"may",
+    6:"june",
+    7:"july",
+    8:"august",
+    9:"september",
+    10:"october",
+    11:"november",
+    12:"december"
+};
+
+var surfer_levels = [
+    'rookie',
+    'intermediate',
+    'advanced',
+    'experienced',
+    'pro'
+];
+
+function loadResults (data) {
+    var items, markers_data = [];
+    if (data.spots.length > 0) {
+        items = data.spots;
+
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+
+            if (item.coords[0] != 0 && item.coords[1] != 0) {
+
+                markers_data.push({
+                    lat : item.coords[0],
+                    lng : item.coords[1],
+                    title : item.name
+                });
+            }
+        }
+    }
+
+    map.addMarkers(markers_data);
+
+    // тоже про умную геолокацию
+    /*
+    google.maps.event.addListenerOnce(map, 'bounds_changed', function(){
+        alert(this.getBounds());
+    });
+
+    for (var i=0; i<markers_data.length; i++){
+        if( map.getBounds().contains(markers_data[i].getPosition()) ){
+            console.log(markers_data[i])
+        }
+    }*/
+}
+
+function initMap(){
+
+    var $MAP = $('#map');
+    map = new GMaps({
+        div: '#map',
+        lat: 43,
+        lng: 25,
+        zoom: 2,
+        markerClusterer: function(map) {
+            options = {
+                gridSize: 40
+            }
+
+            return new MarkerClusterer(map, [], options);
+        }
+    });
+    $MAP.removeClass('hidden');
+
+    // умную геолокацию оставим на потом
+    /*
+    GMaps.geolocate({
+        success: function(position) {
+            map.setCenter(position.coords.latitude, position.coords.longitude);
+        },
+        error: function(error) {
+            alert('Geolocation failed: '+error.message);
+            map.setZoom(2)
+        },
+        not_supported: function() {
+            alert("Your browser does not support geolocation");
+            map.setZoom(2)
+        },
+        always: function() {
+            $MAP.removeClass('hidden');
+        }
+    });
+    */
+
+    var xhr = $.getJSON('spots.json');
+
+    xhr.done(loadResults);
+}
+
+$(document).ready(function(){
+
+    initMap();
+    initFilters();
+	
+});
+
+
+
+var showFilters = function(){
+    $('.show_filters').addClass('hidden');
+    $('.filters_holder').removeClass('hidden');
+};
+var hideFilters = function(){
+    $('.show_filters').removeClass('hidden');
+    $('.filters_holder').addClass('hidden');
+};
+
+function initFilters(){
+    var $showBtn = $('.show_filters'),
+        $hideBtn = $('.hide_filters'),
+        $filtersHolder = $('.filters_holder');
+        $filtersBlock = $filtersHolder.find('.filters');
+
+    $showBtn.click(showFilters);
+    $hideBtn.click(hideFilters);
+
+    $filtersBlock.css('max-height',$(window).height()-93);
+
+    $('.slider-input-wave-size').jRange({
+        from: 0,
+        to: 20,
+        step: 0.5,
+        scale: [0,5,10,15,'20+'],
+        format: '%s feet',
+        width: 300,
+        showLabels: true,
+        isRange : true
+    });
+
+    $('.slider-input-season').jRange({
+        from: 1,
+        to: 12,
+        step: 1,
+        format: function(value, pointer){
+            return months[value];
+        },
+        width: 300,
+        showLabels: true,
+        isRange : true,
+        snap:true,
+        showScale:false
+    });
+
+    $('.slider-input-level').jRange({
+        from: 0,
+        to: 4,
+        step: 1,
+        format: function(value, pointer){
+            return surfer_levels[value];
+        },
+        width: 300,
+        showLabels: true,
+        isRange : true,
+        snap:true,
+        showScale:false
+    });
+}
+
+
+
